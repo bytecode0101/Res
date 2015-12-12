16405c49b199746ea8fade1d96c22460c8b38f09
sallysway <ioanaandreea476@gmail.com>
2015-12-12 14:53:22 +0200

    Added classes template

---
+++ Res/Client.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Reservations
+{
+    public abstract class Client
+    {
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public Guid Id { get; set; }
+
+        public string Password { get; set; }
+
+        public DateTime DateBirth { get; set; }
+
+        public string Tel { get; set; }
+
+        public List<Guid> Reservations { get; set; }
+
+        public enum Gender { }
+
+        public string Email { get; set; }
+
+
+        public void Authenticate()
+        {
+
+        }
+
+        public void MakeReservation()
+        {
+          
+        }
+
+        public void CancelReservation()
+        {
+            
+        }
+
+        public void ViewCalendar()
+        {
+            
+        }
+
+        public void SubscribeToWaitingList()
+        {
+            
+        }
+    }
+}
---
+++ Res/Item.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Reservations
+{
+    public abstract class Item
+    {
+        private List<Guid> waitingList;
+
+        public string Description { get; set; }
+
+        public Guid Id { get; set; }
+
+        private List<Guid> participants;
+
+        public int NoOfPlaces { get; set; }
+
+        public enum Location { }
+
+        public DateTime DateAndTime { get; set; }
+
+        public void View()
+        {
+
+        }
+
+        public void CreateItem()
+        {
+
+        }
+
+        public void DeleteItem()
+        {
+
+        }
+
+
+
+    }
+}
